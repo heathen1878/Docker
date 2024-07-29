670040d6af80bfda15287f1a5a77ae7f135f3a53
Dom Clayton <dominic@booandjoel.co.uk>
2024-07-29 18:00:22 +0100

    Updated index.js

--- projects/multi_tier_app/src/index.js
+++ projects/multi_tier_app/src/index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 
-const client = redis.createClient({
-    host: 'redis-server', // as defined in the docker compose file
-    port: 6379,
-});
+const client = redis.createClient(
+    {
+        host: 'redis-server', // as defined in the docker compose file
+        port: 6379,
+    }
+);
 
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    res.send('Hi! This web app logs your visits');
-    client.get('visits', (err, visits) => {
-        res.send('Number of visits is: ' + visits);
-        client.set('visits', parseInt(visits) + 1);
-    });
+    client.get(
+        'visits', (err, visits) => {
+            res.send('Number of visits is: ' + visits);
+            client.set('visits', parseInt(visits) + 1);
+        }
+    );
 });
 
 app.listen(8080, () => {
