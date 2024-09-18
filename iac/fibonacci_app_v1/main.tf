909e778569b2c43fb9d7e36d831855f4fb9ad10e
Dom Clayton <dominic@booandjoel.co.uk>
2024-09-18 19:26:41 +0100

    Updated IaC for container apps

--- iac/fibonacci_app_v1/main.tf
+++ iac/fibonacci_app_v1/main.tf
@@ -1,15 +1,20 @@
 locals {
   # define some local variables
-  resource_group_name     = format("rg-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  app_service_plan        = format("plan-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  client_app_service_name = format("app-client-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  api_app_service_name    = format("app-api-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  worker_app_service_name = format("app-worker-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  redis_cache             = format("redis-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  postgresql              = format("psql-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
-  name                    = "frontend"
-  location                = "uksouth"
-  random                  = random_id.this.hex
+  resource_group_name            = format("rg-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  virtual_network_name           = format("vnet-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  cae_infra_resource_group_name  = format("rg-cae-infra-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  container_app_environment_name = format("cae-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  api_container_app              = format("ca-api-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  client_container_app           = format("ca-client-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  worker_container_app           = format("ca-worker-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  api_docker_image = format("index.docker.io/heathen1878/api:%s", var.docker_image_tag)
+  client_docker_image = format("index.docker.io/heathen1878/client:%s", var.docker_image_tag)
+  worker_docker_image = format("index.docker.io/heathen1878/worker:%s", var.docker_image_tag)
+  redis_cache                    = format("redis-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  postgresql                     = format("psql-%s-%s-%s-%s", local.name, var.environment, local.location, local.random)
+  name                           = "fibonacci"
+  location                       = "uksouth"
+  random                         = random_id.this.hex
   tags = {
     managedby   = "Terraform"
     pipeline    = "Github Actions"
@@ -28,15 +33,101 @@ resource "azurerm_resource_group" "this" {
   tags     = local.tags
 }
 
-
-
-
-resource "azurerm_service_plan" "this" {
-  name                = local.app_service_plan
+resource "azurerm_virtual_network" "this" {
+  name                = local.virtual_network_name
   location            = local.location
   resource_group_name = azurerm_resource_group.this.name
-  os_type             = "Linux"
-  sku_name            = "P0v3"
-  tags                = local.tags
+  address_space = [
+    "192.168.0.0/16"
+  ]
 }
 
+resource "azurerm_subnet" "cae" {
+  name                 = "cae"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes = [
+    "192.168.0.0/21"
+  ]
+}
+
+resource "azurerm_subnet" "pe" {
+  name                 = "pe"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes = [
+    "192.168.8.0/24"
+  ]
+}
+
+resource "azurerm_container_app_environment" "this" {
+  name                               = local.container_app_environment_name
+  location                           = local.location
+  resource_group_name                = azurerm_resource_group.this.name
+  infrastructure_resource_group_name = local.cae_infra_resource_group_name
+  infrastructure_subnet_id           = azurerm_subnet.cae.id
+}
+
+resource "azurerm_container_app" "worker" {
+  name = local.worker_container_app
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name = azurerm_resource_group.this.name
+  revision_mode = "single"
+
+  template {
+    container {
+      name = "worker"
+      image = ""
+      cpu = "0.5"
+      memory = "1.5"
+    }
+  }
+}
+
+resource "azurerm_container_app" "worker" {
+  name = local.worker_container_app
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name = azurerm_resource_group.this.name
+  revision_mode = "single"
+
+  template {
+    container {
+      name = "worker"
+      image = local.worker_docker_image
+      cpu = "0.5"
+      memory = "1.5"
+    }
+  }
+}
+
+resource "azurerm_container_app" "api" {
+  name = local.worker_container_app
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name = azurerm_resource_group.this.name
+  revision_mode = "single"
+
+  template {
+    container {
+      name = "worker"
+      image = local.api_docker_image
+      cpu = "0.5"
+      memory = "1.5"
+    }
+  }
+}
+
+resource "azurerm_container_app" "client" {
+  name = local.worker_container_app
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  resource_group_name = azurerm_resource_group.this.name
+  revision_mode = "single"
+
+  template {
+    container {
+      name = "worker"
+      image = local.client_docker_image
+      cpu = "0.5"
+      memory = "1.5"
+    }
+  }
+}
